43c1b1c743f10796391d91c302d6ed0a489f5f6f
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-11-30 20:43:16 +0800

    modify base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';agriconnect-api.up.railway.app
+const API_BASE_URL = 'https://agriconnect-api.up.railway.app';
 
 // Create axios instance
 const api = axios.create({
